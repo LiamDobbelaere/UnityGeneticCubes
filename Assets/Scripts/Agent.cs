d911669c0f8cc54f18bac02ddeeabadb05423d7b
Tom Dobbelaere <tom.dobbelaere@student.howest.be>
2018-07-02 17:05:12 +0200

    First commit

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+
+    public Genetix.Individual Individual { get; set; }
+    public Transform target;
+
+    private float giveUpTime = 2.5f;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.Find("Target").transform;
+	}
+
+    bool IsGroundInDirection(Vector2 direction)
+    {
+        Vector2 position = transform.position;
+        float distance = 0.3f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        giveUpTime -= Time.deltaTime;
+        if (giveUpTime < 0f)
+        {
+            Individual.Fitness = Vector2.Distance(transform.position, target.position);
+            Individual.Evaluating = false;
+            Destroy(gameObject);
+        }
+
+        var sensorState = new bool[] {
+            IsGroundInDirection(Vector2.up),
+            IsGroundInDirection(Vector2.up + Vector2.right),
+            IsGroundInDirection(Vector2.right),
+            IsGroundInDirection(Vector2.down + Vector2.right),
+            IsGroundInDirection(Vector2.down),
+            IsGroundInDirection(Vector2.down + Vector2.left),
+            IsGroundInDirection(Vector2.left),
+            IsGroundInDirection(Vector2.up + Vector2.left),
+        };
+
+        var xMove = 0;
+        var jump = false;
+
+        for (var i = 0; i < Individual.Chromosome.Length; i += 10)
+        {
+            var sensorsMatch =
+                sensorState[0] == Individual.Chromosome[i + 2] &&
+                sensorState[1] == Individual.Chromosome[i + 3] &&
+                sensorState[2] == Individual.Chromosome[i + 4] &&
+                sensorState[3] == Individual.Chromosome[i + 5] &&
+                sensorState[4] == Individual.Chromosome[i + 6] &&
+                sensorState[5] == Individual.Chromosome[i + 7] &&
+                sensorState[6] == Individual.Chromosome[i + 8] &&
+                sensorState[7] == Individual.Chromosome[i + 9];
+
+            if (sensorsMatch)
+            {
+                xMove = Individual.Chromosome[i] ? 1 : -1;
+                jump = Individual.Chromosome[i + 1];
+                break;
+            }
+        }
+
+        rb.velocity = new Vector2(xMove * 5f, rb.velocity.y);
+
+        if (jump && IsGroundInDirection(Vector2.down))
+            rb.velocity = new Vector2(rb.velocity.x, 5f);
+
+        rb.velocity = new Vector2(rb.velocity.x * 0.9f, rb.velocity.y);
+
+        /*if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.1f)
+            rb.velocity = new Vector2(Mathf.Sign(Input.GetAxisRaw("Horizontal")) * 5f, rb.velocity.y);
+
+        if (Input.GetAxisRaw("Vertical") > 0.1f && IsGroundInDirection(Vector2.down))
+            rb.velocity = new Vector2(rb.velocity.x, 5f);
+
+        rb.velocity = new Vector2(rb.velocity.x * 0.9f, rb.velocity.y);*/
+    }
+}
---
+++ Assets/Scripts/Genetix.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using System.Linq;
+using System;
+
+public class Genetix : MonoBehaviour {
+    enum GenetixPhase
+    {
+        Starting,
+        InitializePopulation,
+        EvaluateFitness,
+        SCM
+    }
+
+    public class Individual : IComparable<Individual>
+    {
+        public float Fitness { get; set; }
+        public bool[] Chromosome { get; set; }
+        public bool Evaluating { get; set; }
+
+        public Individual(bool[] chromosome)
+        {
+            Chromosome = chromosome;
+        }
+
+        public Individual()
+        {
+            Chromosome = RandomChromosome();
+        }
+
+        public Individual[] Crossover(Individual other)
+        {
+            bool[] mixA = new bool[Chromosome.Length];
+            bool[] mixB = new bool[Chromosome.Length];
+
+            for (int i = 0; i < Chromosome.Length; i++)
+            {
+                if (i < Chromosome.Length / 2)
+                {
+                    mixA[i] = Chromosome[i];
+                    mixB[i] = other.Chromosome[i];
+                }
+                else
+                {
+                    mixA[i] = other.Chromosome[i];
+                    mixB[i] = Chromosome[i];
+                }
+            }
+
+            return new Individual[2] { new Individual(mixA), new Individual(mixB) };
+        }
+
+        public void Mutate()
+        {
+            Chromosome[Random.Range(0, Chromosome.Length)] = !Chromosome[Random.Range(0, Chromosome.Length)];
+        }
+
+        private bool[] RandomChromosome()
+        {
+            var b = new bool[240];
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = Random.Range(0, 2) == 0;
+            }
+
+            return b;
+        }
+
+        public int CompareTo(Individual other)
+        {
+            return Fitness.CompareTo(other.Fitness);
+        }
+    }
+
+    public GameObject agentPrefab;
+    public Transform spawnPoint;
+
+    private Individual[] population;
+    private GenetixPhase phase = GenetixPhase.Starting;
+
+    private int populationSize = 200;
+    private int populationTop = 20;
+
+	// Use this for initialization
+	void Start () {
+        population = new Individual[populationSize];
+        InitializePopulation();
+        Evaluate();
+    }
+
+    void InitializePopulation()
+    {
+        phase = GenetixPhase.InitializePopulation;
+        for (int i = 0; i < population.Length; i++)
+        {
+            population[i] = new Individual();
+        }
+    }
+
+    void Evaluate()
+    {
+        phase = GenetixPhase.EvaluateFitness;
+
+        for (int i = 0; i < population.Length; i++)
+        {
+            var agent = Instantiate(agentPrefab, spawnPoint.position, agentPrefab.transform.rotation).GetComponent<Agent>();
+            agent.Individual = population[i];
+            agent.Individual.Evaluating = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (phase == GenetixPhase.EvaluateFitness)
+        {
+            bool phaseOver = true;
+
+            for (int i = 0; i < population.Length; i++)
+            {
+                if (population[i].Evaluating) phaseOver = false;
+            }
+
+            if (phaseOver) phase = GenetixPhase.SCM;
+        }
+
+        if (phase == GenetixPhase.SCM)
+        {
+            Selection();
+            Crossover();
+            Mutation();
+            Evaluate();
+        }
+	}
+
+    void Selection()
+    {
+        SortPopulation();
+
+        //Remove lowest 90%
+        for (int i = populationTop; i < population.Length; i++)
+        {
+            population[i] = null;
+        }
+    }
+
+    void Crossover()
+    {
+        int currentFather = 0;
+        int currentMother = 1;
+
+        //Crossover individuals until population is filled
+        for (int i = populationTop; i < population.Length; i += 2)
+        {
+            Individual[] offspring = population[currentFather].Crossover(population[currentMother]);
+
+            population[i] = offspring[0];
+            population[i + 1] = offspring[1];
+
+            if (++currentMother >= populationTop)
+            {
+                currentFather += 1;
+                currentMother = currentFather + 1;
+            }
+        }
+    }
+
+    void Mutation()
+    {
+        for (int i = 0; i < population.Length; i++)
+        {
+            if (Random.Range(0, 20) == 0) population[i].Mutate();
+        }
+    }
+
+    void SortPopulation()
+    {
+        Array.Sort(population);
+    }
+
+    void PrintPopulation()
+    {
+        for (int i = 0; i < population.Length; i++)
+        {
+            Debug.Log(population[i].Fitness);
+        }
+    }
+}
